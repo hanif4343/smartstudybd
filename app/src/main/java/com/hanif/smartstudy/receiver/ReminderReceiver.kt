3f998d9816f7e94ff873a352e22450721895c2e7
hanif4343 <hsarder43@gmail.com>
2026-05-29 18:05:29 +0600

    🚀 Update via ZIP uploader

---
+++ app/src/main/java/com/hanif/smartstudy/receiver/ReminderReceiver.kt
@@ -0,0 +1,11 @@
+package com.hanif.smartstudy.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ReminderReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // Phase 5 এ পূর্ণ notification আসবে
+    }
+}
