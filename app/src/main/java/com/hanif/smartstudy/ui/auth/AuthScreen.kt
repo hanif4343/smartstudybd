a5fc4485d4d0770a2d8da34d2f9dc5aea4161be3
hanif4343 <hsarder43@gmail.com>
2026-06-06 16:06:21 +0600

    Fix import statements in AuthScreen.kt

--- app/src/main/java/com/hanif/smartstudy/ui/auth/AuthScreen.kt
+++ app/src/main/java/com/hanif/smartstudy/ui/auth/AuthScreen.kt
@@ -12,7 +12,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActionsimport androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
