f28f5ee9c558f57674fe3fd06709ccc83ac84918
hanif4343 <hsarder43@gmail.com>
2026-06-06 10:08:17 +0600

    🚀 Update via ZIP uploader

--- app/src/main/java/com/hanif/smartstudy/SmartStudyApp.kt
+++ app/src/main/java/com/hanif/smartstudy/SmartStudyApp.kt
@@ -1,6 +1,7 @@
 package com.hanif.smartstudy
 
 import android.app.Application
+import com.hanif.smartstudy.util.FcmHelper
 import com.hanif.smartstudy.worker.SyncWorker
 
 class SmartStudyApp : Application() {
@@ -9,5 +10,9 @@ class SmartStudyApp : Application() {
 
         // Phase 3: Periodic content sync + offline queue flush
         SyncWorker.schedulePeriodic(this)
+
+        // FCM token collect করে Firebase RTDB এ save করো
+        // User login থাকলেই token save হবে, না থাকলে login এর পরে save হবে
+        FcmHelper.collectAndSave(this)
     }
 }
